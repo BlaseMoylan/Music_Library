fc6d15f8f2a98fe3fca3857e3eb7e64456f63943
Blase Moylan <moylanblase@gmail.com>
2023-03-13 11:10:13 -0700

    Feat: Set up and structure

--- FlaskRestAPI_PY_Starter-main/app.py
+++ FlaskRestAPI_PY_Starter-main/app.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from dotenv import load_dotenv
 from os import environ
-
+from marshmallow import post_load, fields, ValidationError
 load_dotenv()
 
 # Create App instance
@@ -23,15 +23,75 @@ CORS(app)
 Migrate(app, db)
 
 # Models
+class Song(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(255),nullable=False)
+    artist=db.Column(db.String(255),nullable=False)
+    release_date=db.Column(db.Date,nullable=False)
+    genre=db.Column(db.String(255))
 
+    def __repr__(self):
+        return f'{self.title}{self.artist}{self.release_date}'
 
 
 # Schemas
-
+class SongSchema(ma.Schema):
+    id=fields.Integer(primary_key=True)
+    title=fields.String(required=True)
+    artist=fields.String(required=True)
+    release_date=fields.Date(required=True)
+    genre=fields.String()
+    class Meta:
+        fields=('id','title','artist','release_date','genre')
+    
+    @post_load
+    def create_songs(self,data,**kwargs):
+        return Song(**data)
+song_schema=SongSchema()
+songs_schema=SongSchema(many=True)
 
 
 # Resources
+class SongListResource(Resource):
+    def get(self):
+        all_songs=Song.query.all()
+        return songs_schema.dump(all_songs), 200
 
+    def post(self):
+        try:
+            form_data=request.get_json()
+            new_song=song_schema.load(form_data)
+            db.session.add(new_song)
+            db.session.commit()
+            return song_schema.dump(new_song)
+        except ValidationError as err:
+            return err.messages, 400
+class SongResource(Resource):
+    def get(self,pk):
+        song_from_db=Song.query.get_or_404(pk)
+        return song_schema.dump(song_from_db), 200
+    
+    def delete(self,pk):
+        song_from_db=Song.query.get_or_404(pk)
+        db.session.delete(song_from_db)
+        db.session.commit()
+        return '', 204
 
+    def put(self, pk):
+        song_from_db=Song.query.get_or_404(pk)
+
+        if 'title' in request.json:
+            song_from_db.title=request.json['title']
+        if 'artist' in request.json:
+            song_from_db.artist=request.json['artist']
+        if 'release_date' in request.json:
+            song_from_db.release_date=request.json['release_date']
+        if 'genre' in request.json:
+            song_from_db.genre=request.json['genre']
+        
+        db.session.commit()
+        return song_schema.dump(song_from_db), 200
 
 # Routes
+api.add_resource(SongListResource, '/api/songs')
+api.add_resource(SongResource, '/api/songs/<int:pk>')
